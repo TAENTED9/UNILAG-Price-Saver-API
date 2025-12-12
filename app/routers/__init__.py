d6c35dd861c57c2202487d9cc577d1ad1ca99ec5
TAENTED . <treasurealli33@gmail.com>
2025-12-12 14:19:26 +0100

    Added __init__

---
+++ app/routers/__init__.py
@@ -0,0 +1,13 @@
+"""Routers package for API endpoint modules."""
+
+__all__ = [
+    "items",
+    "prices",
+    "payments",
+    "ml",
+    "pending",
+    "stores",
+    "admin_items",
+    "auth",
+    "google_maps",
+]
